a615e6fa39a7f3aba87f5cbea22ddda431dba3b2
peugusto <pedrooknd@gmail.com>
2026-04-14 20:38:40 -0300

    feat: world cup api is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import GetAllTeams from './util/GetAllTeams'
 
 function App() {
+
+  useEffect(() =>{
+    async function fetchData() {
+          GetAllTeams();
+    }
+
+    fetchData();
+  },[])
 
   return (
     <>
---
+++ src/util/GetAllTeams.js
@@ -0,0 +1,14 @@
+export default async function GetAllTeams(){
+    const content = await fetch("/api/WorldCup/GetAllTeams", {
+        headers:{
+            'git-user':'peugusto',
+        }
+    });
+
+    if(!content.ok){
+        throw new Error("ERROR API")
+    }
+
+    const teams = await content.json();
+    console.log(teams);
+}
