108778f48f7697487d9d5d936cb133127597def2
peugusto <pedrooknd@gmail.com>
2026-04-15 23:52:34 -0300

    fix: fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import GetAllTeams from './util/GetAllTeams'
 import { GetShuffledTeams } from './util/GetShuffledTeams'
 
 function App() {
